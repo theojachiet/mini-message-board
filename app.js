61efee593047c3250784ba956446610f6c3c8f42
Theo <tjachiet@gmail.com>
2026-03-07 23:26:05 +0100

    add new message page and routing + links

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index');
 })
+
+app.get('/new', (req, res) => {
+    res.render('new');
+} );
 
 app.listen(3000, (error) => {
     if (error) throw error;
