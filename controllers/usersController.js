b0fa92aa45e06f64e50fbc6f4c46965ea35dd5c4
Theo <tjachiet@gmail.com>
2026-03-10 17:40:29 +0100

    add update and delete abilities

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -16,5 +16,71 @@ exports.usersCreateGet = (req, res) => {
 exports.usersCreatePost = (req, res) => {
     const { firstName, lastName } = req.body;
     usersStorage.addUser({ firstName, lastName });
-    res.redirect("/");
+    res.redirect("/users");
+};
+
+// FORM VALIDATION
+
+const { body, validationResult, matchedData } = require("express-validator");
+
+const alphaErr = "must only contain letters.";
+const lengthErr = "must be between 1 and 10 characters.";
+
+const validateUser = [
+  body("firstName").trim()
+    .isAlpha().withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 }).withMessage(`First name ${lengthErr}`),
+  body("lastName").trim()
+    .isAlpha().withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`),
+];
+
+// We can pass an entire array of middleware validations to our controller.
+exports.usersCreatePost = [
+  validateUser,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("createUser", {
+        title: "Create user",
+        errors: errors.array(),
+      });
+    }
+    const { firstName, lastName } = matchedData(req);
+    usersStorage.addUser({ firstName, lastName });
+    res.redirect("/users");
+  }
+];
+
+// UPDATE
+exports.usersUpdateGet = (req, res) => {
+  const user = usersStorage.getUser(req.params.id);
+  res.render("updateUser", {
+    title: "Update user",
+    user: user,
+  });
+};
+
+exports.usersUpdatePost = [
+  validateUser,
+  (req, res) => {
+    const user = usersStorage.getUser(req.params.id);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("updateUser", {
+        title: "Update user",
+        user: user,
+        errors: errors.array(),
+      });
+    }
+    const { firstName, lastName } = matchedData(req);
+    usersStorage.updateUser(req.params.id, { firstName, lastName });
+    res.redirect("/users");
+  }
+];
+
+// DELETE
+exports.usersDeletePost = (req, res) => {
+  usersStorage.deleteUser(req.params.id);
+  res.redirect("/users");
 };
