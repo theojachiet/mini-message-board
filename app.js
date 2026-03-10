efa97e45427e2cc889289e93dd90b081d6e30fa2
Theo <tjachiet@gmail.com>
2026-03-10 16:57:56 +0100

    add new routing view and controller files for users

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 const indexRouter = require('./routes/indexRouter');
+const userRouter = require('./routes/userRouter');
 
 app.use('/', indexRouter);
+app.use('/user', userRouter);
 
 app.listen(3000, (error) => {
     if (error) throw error;
