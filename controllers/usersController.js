b0d5a82dbc20d96e7cbbd753180d484a64992d3b
Theo <tjachiet@gmail.com>
2026-03-11 19:59:19 +0100

    connecting db and initializing basic scripts (wip)

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,29 @@
 const usersStorage = require("../storages/usersStorage");
+const db = require("../db/queries");
+
+async function getUsernames(req, res) {
+  const usernames = await db.getAllUsernames();
+  console.log('usernames: ', usernames);
+  res.send("Usernames: " + usernames.map(user => user.username).join(', '));
+}
+
+async function createUsernameGet(req, res) {
+  res.render("createUser", {
+    title: "Create user",
+  });
+}
+
+async function createUsernamePost(req, res) {
+  const { username } = req.body;
+  await db.insertUsername(username);
+  res.redirect("/");
+}
+
+module.exports = {
+  getUsernames,
+  createUsernameGet,
+  createUsernamePost
+};
 
 exports.usersListGet = (req, res) => {
   res.render("users", {
