4ceaaab88b26afe4a99e392d3a8a33fb4446d4ea
Theo <tjachiet@gmail.com>
2026-03-16 18:28:41 +0100

    add middleware passeport funcitons and login form to home page

--- app.js
+++ app.js
@@ -20,7 +20,43 @@ const membersRouter = require('./routes/membersRouter');
 
 app.use('/users', userRouter);
 app.use('/messages', indexRouter);
-app.use('/',membersRouter)
+app.use('/',membersRouter);
+
+//Authntication function
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    }
+  })
+);
+
+// Session functions
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch(err) {
+    done(err);
+  }
+});
 
 
 app.listen(3000, (error) => {
